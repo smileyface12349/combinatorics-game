4021b797ff2203407cb598ca20c70b7f1cbc3c5d
smileyface12349 <josh.humphriss.signup@gmail.coom>
2024-10-16 10:46:46 +0100

    Working prototype for bijections

--- Bijections/match.gd
+++ Bijections/match.gd
@@ -1,31 +1,126 @@
 extends Control
 
+var problem_size = 1
+var bijection: Bijection
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# Initialise the bijection (temp code until levels are created properly)
+	initialise_bijection(problem_size)
+	# Shuffling order currently done inside this function	
+	
 
-var width: int = 10
-var color: Color = Color.GREEN
+func initialise_bijection(problem_size):
+	if problem_size == 1:
+		bijection = Bijection.new(
+			# Left elements
+			[
+				BijectionElement.new("{}", true, 1), 
+			],
+			# Right elements 
+			[
+				BijectionElement.new("0", false, 1),
+			]
+		)
+
+	elif problem_size == 2:
+		bijection = Bijection.new(
+			# Left elements
+			[
+				BijectionElement.new("{}", true, 1), 
+				BijectionElement.new("{1}", true, 2),
+			],
+			# Right elements 
+			[
+				BijectionElement.new("0", false, 1),
+				BijectionElement.new("1", false, 2),
+			]
+		)
+	
+	elif problem_size == 3:
+		bijection = Bijection.new(
+			# Left elements
+			[
+				BijectionElement.new("{}", true, 1), 
+				BijectionElement.new("{1}", true, 2),
+				BijectionElement.new("{2}", true, 3),
+				BijectionElement.new("{1, 2}", true, 4),
+			],
+			# Right elements 
+			[
+				BijectionElement.new("00", false, 1),
+				BijectionElement.new("10", false, 2),
+				BijectionElement.new("01", false, 3),
+				BijectionElement.new("11", false, 4),
+			]
+		)
+		
+	else:	
+		bijection = Bijection.new(
+			# Left elements
+			[
+				BijectionElement.new("{}", true, 1), 
+				BijectionElement.new("{1}", true, 2),
+				BijectionElement.new("{2}", true, 3),
+				BijectionElement.new("{1, 2}", true, 4),
+				BijectionElement.new("{3}", true, 5), 
+				BijectionElement.new("{1, 3}", true, 6),
+				BijectionElement.new("{2, 3}", true, 7),
+				BijectionElement.new("{1, 2, 3}", true, 8),
+			],
+			# Right elements 
+			[
+				BijectionElement.new("000", false, 1),
+				BijectionElement.new("100", false, 2),
+				BijectionElement.new("010", false, 3),
+				BijectionElement.new("110", false, 4),
+				BijectionElement.new("001", false, 5),
+				BijectionElement.new("101", false, 6),
+				BijectionElement.new("011", false, 7),
+				BijectionElement.new("111", false, 8)
+			]
+		)
+		
+	# Shuffle the order for the bijection
+	bijection.from.shuffle()
+	bijection.to.shuffle()
+
+var line_width: int = 8
+var done: bool = false
 
 var mouse_position_drawn: Vector2 = Vector2(0, 0)
 
 @export var font: Font
 
+## An element in a bijection
+##
+## @param text The text to show onscreen
+## @param id   The ID, used to check if the answer is correct. There must be exactly two elements
+##		with each ID (one on each side of the bijection).
+## @param match Which element it is matched to
 class BijectionElement:
 	var text: String
+	var id: int
 	var match: BijectionElement = null
 	var pos: Vector2 = Vector2(0, 0)
-	var size: Vector2 = Vector2(64, 32)
+	var size: Vector2 = Vector2(128, 64)
 	var left: bool
 	
-	func _init(text: String, left: bool) -> void:
+	func _init(text: String, left: bool, id: int) -> void:
 		self.text = text
 		self.left = left
+		self.id = id
 		
 	func is_inside(pos: Vector2) -> bool:
 		return pos.x >= self.pos.x && pos.y >= self.pos.y \
 		 	&& pos.x <= self.pos.x + self.size.x && pos.y <= self.pos.y + self.size.y
+			
+	## Gets the position to draw the line from (the left/right middle)
+	func get_line_pos() -> Vector2:
+		if self.left:
+			return Vector2(self.pos.x + self.size.x, self.pos.y + self.size.y/2)
+		else:
+			return Vector2(self.pos.x, self.pos.y + self.size.y/2)
 
 class Bijection:
 	var from: Array[BijectionElement]
@@ -43,23 +138,36 @@ class Bijection:
 			return self.from
 		else:
 			return self.to
+			
+	## Draw the elements. This:
+	##  - Computes the position for each element
+	##  - Populates element.pos with the position
+	##  - Calls draw_element for each element
+	func draw_elements(draw_element: Callable, spacing: int = 16, 
+			start_left: Vector2 = Vector2(32, 32), start_right: Vector2 = Vector2(1000, 32),
+			mouse_position: Vector2 = Vector2(0, 0)) -> void:
+		# Draw LHS
+		var pos := start_left
+		for element: BijectionElement in self.from:
+			element.pos = pos
+			draw_element.call(element, element.is_inside(mouse_position))
+			pos += Vector2(0, element.size.y + spacing)
+			
+		# Draw RHS
+		pos = start_right
+		for element: BijectionElement in self.to:
+			element.pos = pos
+			draw_element.call(element, element.is_inside(mouse_position))
+			pos += Vector2(0, element.size.y + spacing)
+			
+	## Checks if the current matching is correct
+	func check() -> bool:
+		for element in self.from:
+			if element.match == null || element.match.id != element.id:
+				return false
+		return true
 		
-var bijection = Bijection.new(
-	# Left elements
-	[
-		BijectionElement.new("{}", true), 
-		BijectionElement.new("{1}", true),
-		BijectionElement.new("{2}", true),
-		BijectionElement.new("{1, 2}", true),
-	],
-	# Right elements 
-	[
-		BijectionElement.new("00", false),
-		BijectionElement.new("10", false),
-		BijectionElement.new("01", false),
-		BijectionElement.new("11", false)
-	]
-)
+
 
 var active_element: BijectionElement = null
 
@@ -73,6 +181,10 @@ func _input(event) -> void:
 				if element.is_inside(event.position - get_global_position()):
 					# If so, draw a line starting at this element
 					active_element = element
+					# Check if we need to remove any old lines
+					for e in bijection.from:
+						if e == active_element || e.match == active_element:
+							e.match = null
 					break
 		# If we've already selected an element...
 		else:
@@ -83,15 +195,27 @@ func _input(event) -> void:
 					if element.left:
 						element.match = active_element
 					else:
+						# Check if we need to remove an old matching
+						for e in bijection.from:
+							if e.match == element:
+								e.match = null
+						# Add the new matching
 						active_element.match = element
-					print("Matched " + element.text + " to " + active_element.text)
 					active_element = null
+					check_bijection()
 					break
 			# If we missed, just clear the active element anyway
 			active_element = null
 		# Whatever happened, it's time for a redraw
 		queue_redraw()
 
+func check_bijection():
+	if bijection.check():
+		done = true
+		await get_tree().create_timer(0.5).timeout
+		problem_size += 1
+		initialise_bijection(problem_size)
+		done = false
 
 func _process(_delta):
 	var mouse_position = get_viewport().get_mouse_position() - get_global_position()
@@ -100,27 +224,30 @@ func _process(_delta):
 		queue_redraw()
 
 func _draw():
+		
+	# Define function to draw an element after its position has been assigned
+	var draw_element = func (element: BijectionElement, hover: bool = false):
+		# Draw background for button
+		draw_rect(Rect2(element.pos, element.size), Color.ALICE_BLUE if hover else Color.WHITE, true)
+		# Draw outline for button
+		draw_rect(Rect2(element.pos, element.size), Color.BLUE if hover else Color.BLACK, false)
+		# Position of text is the bottom-left corner
+		draw_string(font, element.pos + Vector2(0, element.size.y/2), element.text, HORIZONTAL_ALIGNMENT_CENTER, -1, 24, Color(0, 0, 0))
 
-	# Draw LHS
-	var pos := Vector2(32, 32)
-	for element: BijectionElement in bijection.from:
-		draw_string(font, pos, element.text, HORIZONTAL_ALIGNMENT_CENTER, -1, 24, Color(0, 0, 0))
-		element.pos = pos
-		pos += Vector2(0, 64)
-		
-	# Draw RHS
-	pos = Vector2(1000, 32)
-	for element: BijectionElement in bijection.to:
-		draw_string(font, pos, element.text, HORIZONTAL_ALIGNMENT_CENTER, -1, 24, Color(0, 0, 0))
-		element.pos = pos
-		pos += Vector2(0, 64)
-		
+	# Draw the elements
+	bijection.draw_elements(
+		draw_element,
+		16,
+		Vector2(32, 32),
+		Vector2(900, 32),
+		mouse_position_drawn
+	)
+	
 	# Draw matchings that have already been done
 	for element: BijectionElement in bijection.from:
 		if element.match != null:
-			draw_line(element.pos, element.match.pos, Color.RED, width)
+			draw_line(element.get_line_pos(), element.match.get_line_pos(), Color.GREEN if done else Color.RED, line_width)
 			
 	# Draw active matching (if applicable)
 	if active_element != null:
-		draw_line(active_element.pos, mouse_position_drawn, Color.GREEN, width)
-		
+		draw_line(active_element.get_line_pos(), mouse_position_drawn, Color.BLUE, line_width)
